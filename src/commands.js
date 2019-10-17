458c18c7a33631118119a023d3aa22956417373a
Samuel Deodato Luiz Costa <samuel@firmamento.com.br>
2019-10-17 19:12:23 -0300

    coisas

--- src/commands.js
+++ src/commands.js
@@ -1,67 +1,51 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 import chalk from 'chalk';
 import fs from "fs";
 import ncp from "ncp";
 import { promisify } from "util";
 import { _new } from './_new';
-export var access = promisify(fs.access);
-export var copy = promisify(ncp);
-export function commandFactory(command, args) {
-    return __awaiter(this, void 0, void 0, function () {
-        var _a;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
-                case 0:
-                    _a = command;
-                    switch (_a) {
-                        case "n": return [3 /*break*/, 1];
-                        case "new": return [3 /*break*/, 1];
-                    }
-                    return [3 /*break*/, 3];
-                case 1: return [4 /*yield*/, _new(args)];
-                case 2:
-                    _b.sent();
-                    return [3 /*break*/, 4];
-                case 3:
-                    console.error("%s Invalid parameter", chalk.red.bold("ERROR"));
-                    _b.label = 4;
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
+import path from 'path';
+import mkdirp from 'mkdirp';
+export const access = promisify(fs.access);
+export const copy = promisify(ncp);
+export const mkdir = promisify(mkdirp);
+export async function commandFactory(command, args) {
+    switch (command) {
+        case "n":
+        case "new":
+            await _new(args);
+            break;
+        case "g":
+        case "get":
+            await _routes(args);
+            break;
+        default:
+            console.error("%s Invalid parameter", chalk.red.bold("ERROR"));
+    }
+}
+async function _routes(args) {
+    try {
+        const dir = args._[1].toLowerCase();
+        if (!dir.match(/^\.?[a-z/]*$/g)) {
+            console.error("%s não é um diretorio válido", chalk.red.bold("ERROR"));
+            process.exit(1);
+        }
+        if (fs.existsSync(dir)) {
+            console.error("%s rota já existente", chalk.red.bold("ERROR"));
+            process.exit(1);
+        }
+        if (!dir) {
+            console.error("%s falta parametro", chalk.red.bold("ERROR"));
+            process.exit(1);
+        }
+        const currentFileUrl = import.meta.url;
+        const templateDir = path.resolve(new URL(currentFileUrl).pathname.slice(1), "../../template/routes/get.ts");
+        await mkdir(`./routes/${dir}`);
+        await copy(templateDir, `./routes/${dir}/get.ts`, { clobber: false });
+        const route_text = fs.readFileSync(`./routes/route.ts`).toString();
+        //const routes = JSON.parse(route_text.match(/export[ ]*const[ ]*routes.*=[ ]*(\[[\s\S]*\])/im)[1]);
+        console.log(route_text);
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
--- src/commands.ts
+++ src/commands.ts
@@ -4,10 +4,12 @@ import fs from "fs";
 import ncp from "ncp";
 import { promisify } from "util";
 import { _new } from './_new';
+import path from 'path'
+import mkdirp from 'mkdirp'
 
 export const access = promisify(fs.access);
 export const copy = promisify(ncp);
-
+export const mkdir = promisify(mkdirp);
 
 
 export async function commandFactory(command:string,args:arg.Result<any>){
@@ -16,17 +18,43 @@ export async function commandFactory(command:string,args:arg.Result<any>){
         case "new":
             await _new(args)
             break;
-            case "get":
-                await _get(args);
-                break;
+        case "g":
+        case "get":
+            await _routes(args);
+            break;
         default:
             console.error("%s Invalid parameter", chalk.red.bold("ERROR"));
     }
 }
 
 
-function _get(args:arg.Result<any>){
+async function _routes(args:arg.Result<any>){
+    try{
+        const dir = args._[1].toLowerCase();
+        if(!dir.match(/^\.?[a-z/]*$/g)){
+            console.error("%s não é um diretorio válido", chalk.red.bold("ERROR"));
+            process.exit(1);
+        }
+        if(fs.existsSync(dir)){
+            console.error("%s rota já existente", chalk.red.bold("ERROR"));
+            process.exit(1);
+        }
+        if(!dir){
+            console.error("%s falta parametro", chalk.red.bold("ERROR"));
+            process.exit(1);
+        }
+        const currentFileUrl = import.meta.url;
+        const templateDir = path.resolve(new URL(currentFileUrl).pathname.slice(1), "../../template/routes/get.ts")
+        await mkdir(`./routes/${dir}`)
+        await copy(templateDir, `./routes/${dir}/get.ts`, { clobber: false });
+        const route_text = fs.readFileSync(`./routes/route.ts`).toString();
+        //const routes = JSON.parse(route_text.match(/export[ ]*const[ ]*routes.*=[ ]*(\[[\s\S]*\])/im)[1]);
+        console.log(route_text);
+    }catch(e){
+        console.log(e)
+    }
     
+
 }
 
 
